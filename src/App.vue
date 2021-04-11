087766163b17a66d2e41a1441b03e6e05be61d25
shakil <sh9743441@gmail.com>
2021-04-11 03:32:07 -0700

    Setup project

---
+++ src/App.vue
@@ -0,0 +1,11 @@
+<template>
+  <h2 class="text-[green] text-3xl ml-[50px]">Hellow world</h2>
+  <h2 class="text-red-500">Hellow world</h2>
+</template>
+
+<script setup>
+import HelloWorld from "./components/HelloWorld.vue";
+
+// This starter template is using Vue 3 experimental <script setup> SFCs
+// Check out https://github.com/vuejs/rfcs/blob/script-setup-2/active-rfcs/0000-script-setup.md
+</script>
